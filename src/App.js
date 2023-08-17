65a536a6b0534bbb95f2f15128c194070964e2c5
Dev_Avishka <dabdissanayake@gmail.com>
2023-08-17 20:50:02 +0530

    lloo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Navigation from './Navigation';
+import ImageWithText from './ImageWithText';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    
+      <Navigation />   
+      <ImageWithText />
+
+      <br></br>
+      <br></br>
+      <br></br>
+
+      <br></br>
+      <center>
+        <h3>
+          ©2023 Dev Avishka
+        </h3>
+      </center>
     </div>
   );
 }
